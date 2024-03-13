4bb966546bc2be1488b08381be19f3c6196f1d96
shivanssharma <124043448+shivanssharma@users.noreply.github.com>
2024-03-13 21:39:39 +0530

    Delete src directory

--- src/components/avatar.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function Avatar(props){
-    return <img src={props.img}  alt="photo1"/>
-}
-
-export default Avatar;
--- src/components/details.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function Details(props){
-    return <p>details is {props.detailsinfo}</p>
-}
-
-export default Details;
